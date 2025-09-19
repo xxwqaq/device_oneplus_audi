0ead4fee5387bc5fe4abdbd8fc0f00f4a6fd79d2
DillerOFire <niktofe1@gmail.com>
2025-09-19 15:19:08 +1000

    audi: torch: Cut max brightness to 40%

--- camera/CameraProviderExtension.cpp
+++ camera/CameraProviderExtension.cpp
@@ -54,7 +54,7 @@ int32_t getTorchMaxStrengthLevelExt() {
   // In our device, both LEDs has same maximum value
   // so get from one.
   auto node = kTorchLedPaths[0] + "/" + TORCH_MAX_BRIGHTNESS;
-  return get(node, 0);
+  return get(node, 0) * 0.6;
 }
 
 int32_t getTorchStrengthLevelExt() {
