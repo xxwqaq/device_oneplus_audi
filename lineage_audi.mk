8f87308e891085d3f2ce1ff7d6142e0b136d5741
xiaoxiaoaqaq <106440884+xiaoxiaoaqaq@users.noreply.github.com>
2026-01-04 17:15:06 +0800

    Update common configuration inheritance in mk file

--- lineage_audi.mk
+++ lineage_audi.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/audi/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_audi
 PRODUCT_DEVICE := audi
