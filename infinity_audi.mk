18de395ee4a1bb7bb70ff1a62c76123cc6e6c41e
xxw <3219634304@qq.com>
2026-01-17 17:50:57 +0800

    Fix merge conflicts

---
+++ infinity_audi.mk
@@ -0,0 +1,36 @@
+#
+# SPDX-FileCopyrightText: 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from audi device
+$(call inherit-product, device/oneplus/audi/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_audi
+PRODUCT_DEVICE := audi
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := PJF110
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DeviceName=OP5CFBL1 \
+    DeviceProduct=PJF110 \
+    SystemDevice=OP5CFBL1 \
+    SystemName=PJF110
+
+INFINITY_MAINTAINER := XiaoXiaoWeiQAQ
+
+#  Under-display fingerprint
+TARGET_HAS_UDFPS := true
+
+# GAPPS
+WITH_GAPPS := true
